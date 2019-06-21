1bdd756e273898116657ed1657c469d63129f6c7
meeae <meeaeking@gmail.com>
2019-06-20 21:32:04 -0500

    add pronunciation and top_example to dictionary app

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -4,12 +4,24 @@ while true
   p "Enter your word (type q to quit):"
   user_input = gets.chomp
 
-  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=a")
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
 
 
-  p response.parse[0]["text"]
-  p response.parse[0]["partOfSpeech"]
+   definition = response.parse[0]["text"]
+  
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/pronunciations?useCanonical=false&limit=50&api_key=")
+
+  pronunciation = response.parse
+
+
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/topExample?useCanonical=false&api_key=")
+  top_example = response.parse[0]
+  
   if user_input == "q"
      break
   end
 end
+
+p definition
+p pronunciation
+p top_example
