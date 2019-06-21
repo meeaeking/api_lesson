948765a5c38380979e294d714e5d56889e823a67
meeae <meeaeking@gmail.com>
2019-06-20 20:19:21 -0500

    loop for user with a new word until they type q

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -4,12 +4,12 @@ while true
   p "Enter your word (type q to quit):"
   user_input = gets.chomp
 
-  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=a")
 
 
   p response.parse[0]["text"]
   p response.parse[0]["partOfSpeech"]
-
-  
-
+  if user_input == "q"
+     break
+  end
 end
